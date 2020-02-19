aaed503ab323d493e12f7b0248da48abb43f4e73
Ivo Kolev <ikolev57@gmail.com>
2020-02-18 21:19:29 -0500

    Feat: Added server config, env, and heroku database

---
+++ routes/api/index.js
@@ -0,0 +1,7 @@
+var router = require('express').Router();
+
+// router.use('/users', require('./users'));
+// router.use('/lists', require('./lists'));
+// router.use('/todos', require('./todos'));
+
+module.exports = router;
