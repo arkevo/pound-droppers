2c10d56f34220ad64ae659004dd57dd0ecc3c9d2
arkevo <ikolev57@gmail.com>
2020-02-20 22:09:33 -0500

    Feat: Added Calendar Models and API-Routes

--- routes/api/index.js
+++
@@ -1,7 +0,0 @@
-var router = require('express').Router();
-
-// router.use('/users', require('./users'));
-// router.use('/lists', require('./lists'));
-// router.use('/todos', require('./todos'));
-
-module.exports = router;
