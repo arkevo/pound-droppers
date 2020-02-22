047509945e1b37df3aa6dd8b419795066cd9f4a7
arkevo <ikolev57@gmail.com>
2020-02-22 11:44:11 -0500

    Bugs: fixed bugs to run the server (#8)

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 
 app.use(morgan("dev"));
-app.use(require("./routes/routesRouter"));
+app.use(require("./routes"));
 
 app.listen(process.env.PORT || PORT, () => {
   console.log(`server running on ${process.env.PORT || PORT}`);
