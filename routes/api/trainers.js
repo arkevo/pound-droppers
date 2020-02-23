dda18a513135ecef9a586b14313c7753600ccd65
hycoding51 <yanghuan.bnu@gmail.com>
2020-02-22 21:54:24 -0500

    feat: enable trainer page

--- routes/api/trainers.js
+++ routes/api/trainers.js
@@ -1,17 +1,11 @@
 var router = require("express").Router();
-var models = require('../../models'); 
-var Trainers = models.Trainers; 
+var models = require("../../models");
+var Trainers = models.Trainers;
 
+router.route("/").get(async (req, res) => {
+  const trainers = await Trainers.findAll();
+  console.log(trainers);
+  res.render("trainers", trainers);
+});
 
-router
-  .route("/")
-  .get(async (req, res) => {
-    const trainers = await Trainers.findAll();
-    res.json({ trainers: trainers });
-  });
-
-  module.exports = router;
-
-
-  
-  
+module.exports = router;
