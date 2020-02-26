d044aa0e64c48ce0f2c669c4117ecbf84f3ac9ea
arkevo <ikolev57@gmail.com>
2020-02-25 19:52:24 -0500

    fix: Update dev/prod sequelize configs

--- config/config.js
+++ config/config.js
@@ -1,33 +1,13 @@
 module.exports = {
-  // development: {
-  //   username: "root",
-  //   password: null,
-  //   database: "pound_droppers_db",
-  //   host: "localhost",
-  //   dialect: "mysql"
-  // },
   development: {
-    username: "zm7uztyi3jyhp33p",
-    password: "tiqr9pd0ohlf1bi3",
-    database: "ujuz5sf7p8cq4j5s",
-    host: "l6slz5o3eduzatkw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    username: "root",
+    password: null,
+    database: "pound_droppers_db",
+    host: "localhost",
     dialect: "mysql"
-  },
-  production: {
-    username: process.env.MYSQL_PROD_USER,
-    password: process.env.MYSQL_PROD_KEY,
-    database: process.env.MYSQL_PROD_DBNAME,
-    host: process.env.MYSQL_PROD_HOST,
-    dialect: "mysql",
-    operatorsAliases: false
   },
   production: {
     use_env_variable: "JAWSDB_URL",
     dialect: "mysql"
   }
 };
-
-// process.env.MYSQL_PROD_USER=zm7uztyi3jyhp33p
-// process.env.MYSQL_PROD_KEY=tiqr9pd0ohlf1bi3
-// process.env.MYSQL_PROD_DBNAME=ujuz5sf7p8cq4j5s
-// process.env.MYSQL_PROD_HOST=l6slz5o3eduzatkw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com
