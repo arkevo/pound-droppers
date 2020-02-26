e1b69707c69317f0492ba802efc0ddccab60d9b1
arkevo <ikolev57@gmail.com>
2020-02-25 20:08:40 -0500

    chore: Fix dev/prod sequelize configs for deploying to heroku and developing locally (#14)
    * Adding to heroku
    
    * fix: Update dev/prod sequelize configs

--- config/config.js
+++ config/config.js
@@ -7,20 +7,7 @@ module.exports = {
     dialect: "mysql"
   },
   production: {
-    username: process.env.MYSQL_PROD_USER,
-    password: process.env.MYSQL_PROD_KEY,
-    database: process.env.MYSQL_PROD_DBNAME,
-    host: process.env.MYSQL_PROD_HOST,
-    dialect: "mysql",
-    operatorsAliases: false
-  },
-  production: {
     use_env_variable: "JAWSDB_URL",
     dialect: "mysql"
   }
 };
-
-// process.env.MYSQL_PROD_USER=zm7uztyi3jyhp33p
-// process.env.MYSQL_PROD_KEY=tiqr9pd0ohlf1bi3
-// process.env.MYSQL_PROD_DBNAME=ujuz5sf7p8cq4j5s
-// process.env.MYSQL_PROD_HOST=l6slz5o3eduzatkw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com
