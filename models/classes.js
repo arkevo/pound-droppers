3469905f1b17596d03556ec4eac785c6496f081b
arkevo <ikolev57@gmail.com>
2020-02-25 20:34:03 -0500

    chore: Fix seed file and create npm command for seeding prod db

--- models/classes.js
+++ models/classes.js
@@ -1,30 +1,17 @@
 "use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Trainers = sequelize.define(
-    "Trainers",
-    {
-      name: DataTypes.STRING,
-      photo: DataTypes.STRING,
-      introduction: DataTypes.STRING
-    },
-    {}
-  );
   const Classes = sequelize.define(
     "Classes",
     {
       type: DataTypes.STRING,
       time: DataTypes.DATE,
-      room: DataTypes.STRING,
-      trainerid: DataTypes.INTEGER
+      room: DataTypes.STRING
     },
     {}
   );
   Classes.associate = function(models) {
     // associations can be defined here
-    Trainers.hasMany(Classes, {
-      foreignKey: "trainerid"
-    });
-    Classes.belongsTo(Trainers);
+    Classes.belongsTo(models.Trainers);
   };
   return Classes;
 };
